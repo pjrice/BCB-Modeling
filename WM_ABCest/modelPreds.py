802ef1fef38ee4c17f31dc4034d2064d2f102548
pjrice <pjr@rice.edu>
2022-08-11 13:13:55 -0500

    output model predictions to csv

--- WM_ABCest/modelPreds.py
+++ WM_ABCest/modelPreds.py
@@ -1,4 +1,5 @@
 import os
+import csv
 import numpy as np
 
 os.chdir('/home/ausmanpa/actr7.x/tutorial/python')
@@ -39,6 +40,11 @@ def compute_model_acc (correctCueChunks,chunkRetrievals):
     
     return(accVec)
 
+def compute_kp_acc(kpResp,correctResp):
+    compVec = zip(kpResp,correctResp)
+    accVec = [1 if i==j else 0 for i,j in compVec]
+    return(accVec)
+
 
 def getModelPreds(numRuns):
     
@@ -48,6 +54,16 @@ def getModelPreds(numRuns):
     # establish the "correct" chunk to retrieve on each trial
     correctCueChunks = np.array(['CUE0-0','CUE1-0','CUE2-0','CUE3-0','CUE4-0','CUE5-0','CUE6-0','CUE7-0'])
     correctCueChunks = np.repeat(correctCueChunks,10,axis=0).tolist()
+    
+    # get the correct keypresses and trial types
+    correctRespTTs = []
+    with open('/home/ausmanpa/gp/BCB-Modeling/WM_ABCest/model/zeroBack_correctResponses.csv',newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            correctRespTTs.append(row)
+    
+    correct_kps = [i[0] for i in correctRespTTs]
+    trialTypes = [i[1] for i in correctRespTTs]
     
     # create the stimulus timing for the zero-back condition
     stimTiming = create_stim_timing()
@@ -84,6 +100,7 @@ def getModelPreds(numRuns):
     
         # get model's keypress responses, RTs, and retrieved chunks
         tempRTs = [x[1] for x in modelResponses]
+        keypresses = [x[0] for x in modelResponses]
         chunkRetrievals = [x[2] for x in modelResponses]
     
         # compute RTs by subtracting the timing of the stimuli
@@ -93,10 +110,16 @@ def getModelPreds(numRuns):
         accVec = compute_model_acc(correctCueChunks,chunkRetrievals)
         print('Model accuracy: '+str(np.mean(accVec)))
         
+        kpAcc = compute_kp_acc(keypresses,correct_kps)
+        
         runResults = {}
         runResults['gaVal'] = ga
+        runResults['trialTypes'] = trialTypes
         runResults['RTs'] = modelRespTimes
+        runResults['Keypresses'] = keypresses
+        runResults['kpAcc'] = kpAcc
         runResults['Retrievals'] = chunkRetrievals
+        runResults['retAcc'] = accVec
         
         resultsDict['Run'+str(runNum)] = runResults
         
@@ -104,7 +127,32 @@ def getModelPreds(numRuns):
     
     return(resultsDict)
 
+
+def writeResults2csv(resultsDict,savePath):
     
+    results2write = [['runNum','gaVal','tNum','tType','rt','kp','kpAcc','ret','retAcc']]
+    for runNum in list(resultsDict.keys()):
+        gaVal = resultsDict[runNum]['gaVal']
+        trialTypes = resultsDict[runNum]['trialTypes']
+        rts = resultsDict[runNum]['RTs']
+        kps = resultsDict[runNum]['Keypresses']
+        kpAcc = resultsDict[runNum]['kpAcc']
+        ret = resultsDict[runNum]['Retrievals']
+        retAcc = resultsDict[runNum]['retAcc']
+        
+        tNum = range(1,len(resultsDict[runNum]['RTs'])+1)
+        
+        runResults = [[runNum,gaVal,tNum[i],trialTypes[i],rts[i],kps[i],kpAcc[i],ret[i],retAcc[i]] for i in range(0,len(tNum))]
+        
+        for tResult in runResults:
+            results2write.append(tResult)
+            
+    with open(savePath,'w',newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(results2write)
+            
+    
+        
         
         
     
