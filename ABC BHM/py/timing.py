21b4bdbd3f068150c46e1ff2a5861adde4e402c9
pjrice <pjrice@uw.edu>
2023-05-13 08:58:56 -0400

    parameter recovery testing

--- ABC BHM/py/timing.py
+++ ABC BHM/py/timing.py
@@ -6,10 +6,21 @@
 import time
 
 tic = time.time()
-abc_hierarchical_model(0.2,numChainIter=100)
+tuning20 = abc_hierarchical_model(0.2,numChainIter=10000)
+tuning20.to_csv('Z:\\gp\\BCB-Modeling\\ABC BHM\\py\\pyData\\parameterRecovery\\rtAndAcc\\tuning20.csv')  
 toc = time.time()
 
 toc - tic
+
+
+tuning05 = abc_hierarchical_model(0.05,numChainIter=10000)
+tuning05.to_csv('Z:\\gp\\BCB-Modeling\\ABC BHM\\py\\pyData\\parameterRecovery\\rtAndAcc\\tuning05.csv')  
+
+tuning10 = abc_hierarchical_model(0.10,numChainIter=10000)
+tuning10.to_csv('Z:\\gp\\BCB-Modeling\\ABC BHM\\py\\pyData\\parameterRecovery\\rtAndAcc\\tuning10.csv') 
+
+tuning15 = abc_hierarchical_model(0.15,numChainIter=10000)
+tuning15.to_csv('Z:\\gp\\BCB-Modeling\\ABC BHM\\py\\pyData\\parameterRecovery\\rtAndAcc\\tuning15.csv') 
 
 
 # time to run abc_hierarchical_model with 100 chain iterations:
